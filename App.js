fc8f41a481b7caeaf3ad431a8165b64e2bfe75b9
MTAREK19 <moubariki.tarik@gmail.com>
2021-06-02 11:50:20 +0100

    done

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React,{Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {createAppContainer,createSwitchNavigator} from 'react-navigation';
+// import {createAppContainer} from 'react-navigation';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import 'react-native-gesture-handler';
 
-import LoadingScreen from './screens/LoginScreen';
+
+import DetailsCurrencyScreen from './screens/DetailsCurrencyScreen';
 import LoginScreen from './screens/LoginScreen';
-import DashboardScreen from './screens/DashboardScreen';
+import DashboardCurrencyScreen from './screens/DashboardCurrencyScreen';
+import HomeScreen from './screens/HomeScreen';
+import Chart from './screens/Chart';
+import Wallet from './screens/WalettScreen'
 
 import * as firebase from 'firebase';
 import { firebaseConfig } from './firebaseConfig';
@@ -14,17 +21,94 @@ class App extends Component {
 
 render(){
   return (
-         <AppNavigator/>
+    <NavigationContainer>
+
+    <Stack.Navigator initialRouteName="Sign In">
+      <Stack.Screen name="Sign In" component={LoginScreen}  
+      options={{
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}/>
+
+
+      <Stack.Screen name="HomeScreen" component={HomeScreen}
+       options={{
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }} 
+      />
+
+
+      <Stack.Screen name="Chart" component={Chart} 
+       options={{
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}/>
+
+
+      <Stack.Screen name="DashboardCurrencyScreen" component={DashboardCurrencyScreen}
+       options={{
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }} />
+
+
+      <Stack.Screen name="DetailsCurrencyScreen" component={DetailsCurrencyScreen} 
+       options={{
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}/>
+      
+      <Stack.Screen name="Wallet" component={WalletScreen} 
+       options={{
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}/>
+    </Stack.Navigator>
+  </NavigationContainer>
     )
   }
 }
 
-const AppSwitchNavigator = createSwitchNavigator({
-  LoadingScreen: LoadingScreen,
-  LoginScreen: LoginScreen,
-  DashboardScreen: DashboardScreen
- })
- const AppNavigator = createAppContainer(AppSwitchNavigator)
+const Stack = createStackNavigator();
+
+
 
 
 const styles = StyleSheet.create({
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View,Image, ToastAndroid } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 import { Button } from 'react-native-elements';
 import firebase from 'firebase';
 import * as Google from 'expo-google-app-auth';
 import Icon from 'react-native-vector-icons/FontAwesome';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
 
-
-
-
- class LoginScreen extends Component {
+const LoginScreen = ({ navigation }) => {
 
   isUserEqual = (googleUser, firebaseUser) => {
     if (firebaseUser) {
@@ -17,6 +15,7 @@ import Icon from 'react-native-vector-icons/FontAwesome';
         if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
             providerData[i].uid === googleUser.getBasicProfile().getId()) {
           // We don't need to reauth the Firebase connection.
+          // getBasicProfile().getId()
           return true;
         }
       }
@@ -26,7 +25,7 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 
    onSignIn = googleUser => {
-    console.log('Google Auth Response', googleUser);
+    console.log('=========Google Auth Response==========', googleUser);
     // We need to register an Observer on Firebase Auth to make sure auth is initialized.
     var unsubscribe = firebase
       .auth()
@@ -43,7 +42,9 @@ import Icon from 'react-native-vector-icons/FontAwesome';
         firebase
         .auth()
         .signInWithCredential(credential).then(function(){
-         console.log('user signed in ');
+          showToastWithGravity();
+          console.log('======= user signed in successfully =======');
+          
 
 
         })
@@ -58,7 +59,10 @@ import Icon from 'react-native-vector-icons/FontAwesome';
           // ...
         });
       } else {
-        console.log('User already signed-in Firebase.');
+
+        console.log('========User already signed-in Firebase=========');
+       
+
       }
     }.bind(this));
   }
@@ -69,15 +73,18 @@ import Icon from 'react-native-vector-icons/FontAwesome';
     try {
       const result = await Google.logInAsync({
         behavior:'web',
-        androidClientId: '1042090710311-73t61259obl4gfch27944j190j7u3oap.apps.googleusercontent.com',
+        androidClientId: '607696297500-d29dk1ssear0btfbo847146s9rv8lvev.apps.googleusercontent.com',
         // iosClientId: YOUR_CLIENT_ID_HERE,
         scopes: ['profile', 'email'],
       });
   
       if (result.type === 'success') {
         this.onSignIn(result);
+
+        navigation.navigate('HomeScreen');
         return result.accessToken;
       } else {
+        navigation.navigate('LoginScreen');
         return { cancelled: true };
       }
     } catch (e) {
@@ -86,56 +93,61 @@ import Icon from 'react-native-vector-icons/FontAwesome';
   }
 
 
-    render() {
+
 
         return (
             <View style={styles.container}>
+             <Text style={styles.title}>Welcome to our App</Text>
 
                 <Image
                      style={styles.logo}
-                      source={require('../assets/logo.png')}
+                      source={require('../assets/Bitcoin.png')}
                 />
 
 
           <Button
+           style={styles.buttonGoogle}
            icon={
             <Icon
               name="google"
-              size={30}
-             
+              size={22}
+              color="white"
             />
           }
        
               onPress={() => this.signInWithGoogleAsync()}
               title=" sign In with Google"
-              
+              color="black"
 /> 
         
-        
+
 
 
             </View> 
         )
     }
-}
+
 const styles = StyleSheet.create({
 
     container: {
       flex: 1,
-      backgroundColor: 'white',
       alignItems: 'center',
       justifyContent: 'center',
 },
-buttonGoogle: {
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    borderRadius: 5,
-    width:'70%', 
-    color: '#ffffff',
-    backgroundColor: 'transparent',
-    marginBottom:1   
-  },
-
+title:{
+     fontSize:25,
+     color:'#f4511e',
+     fontWeight:'bold'
+},
+// buttonGoogle: {
+//     paddingHorizontal: 20,
+//     paddingVertical: 30,
+//     borderRadius: 15,
+//     width:'70%', 
+//     color: '#ffffff',
+//     backgroundColor: 'transparent',
+//     marginBottom:11   
+//   },
   logo: {
     width: 340,
     height: 340,
